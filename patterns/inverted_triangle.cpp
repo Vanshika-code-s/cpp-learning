7c4cf1d6faa7f9314a74d5397c5626a87ea8707b
Vanshika <vanshikasehrawat1411@gmail.com>
2026-07-06 16:16:07 +0530

    addedhollow diamond , pyramid pattern

--- patterns/inverted_triangle.cpp
+++ patterns/inverted_triangle.cpp
@@ -4,12 +4,13 @@ int main() {
     int n;
     cout << "Enter the number of rows: ";
     cin >> n;
+    char num='a';
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < i; j++) {
             cout << " ";
         }
         for (int j = 0; j < n - i; j++) {
-            cout << i + 1;
+            cout << char(num+i) ;
         }
         cout << endl;
     }
